0b3506c0adf2676175ac627309ee3ab25785517b
pallavis57 <spallavi2303@gmail.com>
2025-01-20 11:44:54 +0530

    Initial commit with UI files

---
+++ js/custom/dashboard.js
@@ -0,0 +1,22 @@
+$(function () {
+    //Json data by api call for order table
+    $.get(orderListApiUrl, function (response) {
+        if(response) {
+            var table = '';
+            var totalCost = 0;
+            $.each(response, function(index, order) {
+                if (!order.customer_name || order.customer_name.trim() === "") {
+                    order.customer_name = "Unknown";  // Default to "Unknown" if no customer name
+                }
+                totalCost += parseFloat(order.total);
+                table += '<tr>' +
+                    '<td>'+ order.datetime +'</td>'+
+                    '<td>'+ order.order_id +'</td>'+
+                    '<td>'+ order.customer_name +'</td>'+
+                    '<td>'+ order.total.toFixed(2) +' Rs</td></tr>';
+            });
+            table += '<tr><td colspan="3" style="text-align: end"><b>Total</b></td><td><b>'+ totalCost.toFixed(2) +' Rs</b></td></tr>';
+            $("table").find('tbody').empty().html(table);
+        }
+    });
+});
